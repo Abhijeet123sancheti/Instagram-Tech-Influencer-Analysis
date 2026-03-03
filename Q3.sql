d1b0bc4854f7f392cd56a3ab619ee5a55b27a00a
Abhijeet123sancheti <109809607+Abhijeet123sancheti@users.noreply.github.com>
2026-03-03 15:45:28 +0530

    Refactor SQL query to filter weekend posts in March and April

--- Q3.sql
+++ Q3.sql
@@ -1,11 +1,11 @@
 # 3. Filter all the posts that were published on a weekend in the month of March and April and export them to a separate csv file.
 
-SELECT content.* 
-FROM gdb0120.dim_dates dates
-INNER JOIN 
-gdb0120.fact_content content
-ON dates.date = content.date
-WHERE 
-(MONTH(dates.date) = "3" OR MONTH(dates.date) = "4")
-AND LOWER(dates.weekday_or_weekend) = "weekend"
-;
+select c.date,d.month_name,  
+d.weekday_or_weekend,c.post_category,c.carousel_item_count,
+c.post_type from gdb0120.fact_content c
+inner join  dim_dates d 
+on d.date = c.date
+where month_name in( "March" ,"April" )
+and weekday_or_weekend = "Weekend";
+ 
+
