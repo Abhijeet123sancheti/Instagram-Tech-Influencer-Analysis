5976d1352540b3525d6389ba3f4391197989591e
Abhijeet123sancheti <109809607+Abhijeet123sancheti@users.noreply.github.com>
2026-03-03 15:43:39 +0530

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -1,8 +1,5 @@
-# 2. What are the highest and lowest recorded impressions for each post type? 
+"#2.What are the highest and lowest recorded impressions for each post type?"
 
-SELECT 
-post_type,
-MAX(impressions) AS "highest recorded impressions",
-MIN(impressions) as "lowest recorded impressions"
-FROM gdb0120.fact_content
-GROUP BY post_type;
+select post_type, min(impressions), max(impressions) 
+from gdb0120.fact_content
+group by post_type ;
