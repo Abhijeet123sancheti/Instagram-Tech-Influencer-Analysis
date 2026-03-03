bd25dc5eb9707393557456d5c4edf0a133d859fd
Abhijeet123sancheti <109809607+Abhijeet123sancheti@users.noreply.github.com>
2026-03-03 15:44:46 +0530

    Fix comment formatting in Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -1,4 +1,4 @@
-"#2.What are the highest and lowest recorded impressions for each post type?"
+#2.What are the highest and lowest recorded impressions for each post type?
 
 select post_type, min(impressions), max(impressions) 
 from gdb0120.fact_content
